be7b9b33aa78f45b697ed7cfe9037bdccf39c0fe
praveenkumar <praveen0077kumar@gmail.com>
2018-03-31 10:15:22 +0530

    video 2.2

--- frontend/src/main/java/net/pk/frontend/controller/PageController.java
+++ frontend/src/main/java/net/pk/frontend/controller/PageController.java
@@ -1,5 +1,7 @@
 package net.pk.frontend.controller;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
